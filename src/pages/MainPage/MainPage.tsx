da438bc2ca54a0278771b434714230acbc7d87c4
Yooters <inglourious.hedgehog@gmail.com>
2024-07-08 21:04:37 +0300

    initial commit

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Counter} from "../../components/Counter";
+
+const MainPage = () => {
+    return (
+        <div>
+            MAIN PAGE
+            <Counter />
+        </div>
+    );
+};
+
+export default MainPage;
