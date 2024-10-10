2ba968764da8c648bbc7c90fe717d7813cb1762b
Yooters <inglourious.hedgehog@gmail.com>
2024-10-10 22:23:13 +0300

    fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,12 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+// паттерн, который отрабатывает перед каким-то действием (перед каждым запросом)
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
