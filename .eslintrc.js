9229ea28aba8f7e30ee4480a86e9e4c394bd07e5
Yooters <inglourious.hedgehog@gmail.com>
2024-11-05 03:11:37 +0300

    add toggle flags and auto remove script

--- .eslintrc.js
+++ .eslintrc.js
@@ -93,6 +93,7 @@ module.exports = {
         ],
         'unused-imports/no-unused-imports': 'error',
         'react/jsx-max-props-per-line': ['error', { maximum: 4 }],
+        'react/no-unstable-nested-components': 'warn',
     },
     globals: {
         __IS_DEV__: true,
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -14,9 +14,9 @@ import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetails
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { articleDetailsPageReducer } from '../../model/slices';
 import cls from './ArticleDetailsPage.module.scss';
-import { getFeatureFlag } from '@/shared/lib/features';
-import { Counter } from '@/entities/Counter';
+import { getFeatureFlag, toggleFeatures } from '@/shared/lib/features';
 import { ArticleRating } from '@/features/articleRating';
+import { Card } from '@/shared/ui/Card';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -37,6 +37,26 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         return null;
     }
 
+    // const CounterRedesigned = () => <div>new counter</div>;
+    // было до  npx ts-node .\scripts\remove-feature.ts isCounterEnabled off
+    // const counter = toggleFeatures({
+    //     name: 'isCounterEnabled',
+    //     on: () => <CounterRedesigned />,  // тут обязательно в одну строку (название компонента/функции/объекта, строка, число, не много строк кода )
+    //     off: () => <Counter />,           // тут обязательно в одну строку (название компонента/функции/объекта, строка, число, не много строк кода )
+    // });
+
+    // стало после off
+    // const counter = <CounterRedesigned />;
+
+    // стало после on
+    // const counter = <Counter />;
+
+    const articleRatingCard = toggleFeatures({
+        name: 'isArticleRatingEnabled',
+        on: () => <ArticleRating articleId={id} />,
+        off: () => <Card>{t('Оценка статей скоро появится')}</Card>,
+    });
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <Page
@@ -45,8 +65,8 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
                 <VStack gap="16" max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    {isCounterEnabled && <Counter />}
-                    {isArticleRatingEnabled && <ArticleRating articleId={id} />}
+                    {articleRatingCard}
+                    {/* {counter} */}
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
